f2e363bab2a76c715166cd6394ee6f613c2042a8
Jonathan Zempel <jzempel@gmail.com>
2019-09-18 15:47:03 -0700

    chore: add SVG linting (#118)

---
+++ .svglintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  rules: {
+    attr: [
+      {
+        'fill-rule': false
+      },
+      {
+        'rule::selector': 'svg',
+        'viewBox': /^0 0/u
+      }
+    ],
+    custom: [
+      (reporter, $, ast) => {
+        const strokeCurrentColor = $.find('[stroke="currentColor"]');
+        const fillCurrentColor = $.find('[fill="currentColor"]');
+
+        if (strokeCurrentColor.length + fillCurrentColor.length === 0) {
+          reporter.error('Missing `currentColor`', null, ast);
+        }
+      }
+    ]
+  }
+};
