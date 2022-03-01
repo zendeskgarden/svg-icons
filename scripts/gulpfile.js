b3fa8869a6e705a93c245a9b7e830a0782ad8f53
Valentin <carvallegro@users.noreply.github.com>
2022-03-02 09:11:01 +1100

    fix: changed component output

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -131,7 +131,8 @@ const transformToComponent = svgCode =>
 *
 * Use of this source code is governed under the Apache License, Version 2.0
 * found at http://www.apache.org/licenses/LICENSE-2.0.
-*/\n`.concat(
+*/
+`.concat(
     transform.sync(
       svgCode,
       {
