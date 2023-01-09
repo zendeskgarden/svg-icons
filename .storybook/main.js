7dc8d8f323dbda0e728027920d8e69cb54d8060b
Mike Tobia <mike.tobia@zendesk.com>
2023-01-09 14:24:48 -0500

    chore(storybook): support node v18 and upgrade to webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,13 @@
 
 module.exports = {
   stories: ['../src/.stories/*.stories.mdx', '../src/.stories/*.stories.js'],
-  addons: [{ name: '@storybook/addon-essentials', options: { actions: false } }]
+  addons: [{ name: '@storybook/addon-essentials', options: { actions: false } }],
+  core: {
+    builder: 'webpack5'
+  },
+  webpackFinal: config => {
+    config.output.hashFunction = 'xxhash64';
+
+    return config;
+  }
 };
