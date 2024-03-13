0acdf2b50ae16d02dd99086a22d1cfd1c80d781a
george treviranus <geowtrev@gmail.com>
2024-03-13 09:35:27 -0500

    chore: updates to CSF 3 style

--- src/.stories/26px.stories.js
+++ src/.stories/26px.stories.js
@@ -6,87 +6,67 @@
  */
 
 import { Col, Grid, Row } from '@zendeskgarden/react-grid';
+import { PALETTE } from '@zendeskgarden/react-theming';
+import styled from 'styled-components';
 import Icon from './Icon';
 import IconGrid from './IconGrid';
-import { PALETTE } from '@zendeskgarden/react-theming';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { SVGs } from './26px';
-import styled from 'styled-components';
+import { sharedArgTypes, sharedPropTypes } from './common';
 
 export default {
   title: '26 px'
 };
 
-const args = {
-  fill: PALETTE.kale[700],
-  size: 26
-};
+const args = { fill: PALETTE.kale[700], size: 26 };
 
 const argTypes = {
-  color: {
-    name: 'Color',
-    control: 'color'
-  },
-  fill: {
-    name: 'Fill',
-    control: 'color'
-  },
+  ...sharedArgTypes,
   size: {
-    name: 'Size',
-    control: { type: 'range', min: 8, max: 104, step: 1 }
+    ...sharedArgTypes.size,
+    control: {
+      ...sharedArgTypes.size.control,
+      max: 104
+    }
   }
 };
 
-export const Icons = ({ color, fill, size }) => (
-  <IconGrid
-    color={color}
-    fill={fill}
-    size={size}
-    svgs={SVGs.filter(value => {
-      return !value.includes('wordmark');
-    })}
-  />
-);
-
-Icons.args = {
-  ...args
+const propTypes = {
+  ...sharedPropTypes
 };
 
-Icons.argTypes = {
-  ...argTypes
+export const Icons = {
+  render: ({ color, fill, size }) => (
+    <IconGrid
+      color={color}
+      fill={fill}
+      size={size}
+      svgs={SVGs.filter(value => {
+        return !value.includes('wordmark');
+      })}
+    />
+  ),
+  args,
+  argTypes,
+  propTypes
 };
 
-Icons.propTypes = {
-  color: PropTypes.string,
-  fill: PropTypes.string,
-  size: PropTypes.number
-};
-
-export const Wordmarks = ({ color, fill, size }) => (
-  <IconGrid
-    color={color}
-    columns={2}
-    fill={fill}
-    size={size}
-    svgs={SVGs.filter(value => {
-      return value.includes('wordmark');
-    })}
-  />
-);
-
-Wordmarks.args = {
-  ...args
-};
-
-Wordmarks.argTypes = {
-  ...argTypes
-};
-
-Wordmarks.propTypes = {
-  color: PropTypes.string,
-  fill: PropTypes.string,
-  size: PropTypes.number
+export const Wordmarks = {
+  render: ({ color, fill, size }) => (
+    <IconGrid
+      color={color}
+      columns={2}
+      fill={fill}
+      size={size}
+      svgs={SVGs.filter(value => {
+        return value.includes('wordmark');
+      })}
+    />
+  ),
+  args,
+  argTypes,
+  propTypes
 };
 
 const StyledSpan = styled.span`
@@ -96,111 +76,104 @@ const StyledSpan = styled.span`
   font-size: ${props => props.size}px;
 `;
 
-export const WordmarkLayouts = ({ color, fill, size }) => (
-  <Grid>
-    {Object.keys(PALETTE.product).map(product => (
-      <Row key={product}>
+export const WordmarkLayouts = {
+  render: ({ color, fill, size }) => (
+    <Grid>
+      {Object.keys(PALETTE.product).map(product => (
+        <Row key={product}>
+          <Col md={6} style={{ marginBottom: 20 }}>
+            <Icon
+              color={color || PALETTE.product[product]}
+              fill={fill}
+              id={`zd-svg-icon-26-relationshape-${product}`}
+              size={size}
+            />
+            <StyledSpan size={size}>
+              <Icon color={color} id={`zd-svg-icon-26-wordmark-${product}`} />
+            </StyledSpan>
+          </Col>
+          <Col md={6} style={{ marginBottom: 20 }}>
+            <Icon color={color} id="zd-svg-icon-26-wordmark-zendesk" size={size} />
+            <span style={{ display: 'inline-block', marginLeft: '.22em', fontSize: size }}>
+              <Icon color={color} id={`zd-svg-icon-26-wordmark-${product}`} />
+            </span>
+          </Col>
+        </Row>
+      ))}
+      <Row>
         <Col md={6} style={{ marginBottom: 20 }}>
           <Icon
-            color={color || PALETTE.product[product]}
+            color={color || PALETTE.product.talk}
             fill={fill}
-            id={`zd-svg-icon-26-relationshape-${product}`}
+            id="zd-svg-icon-26-sunshine"
             size={size}
           />
           <StyledSpan size={size}>
-            <Icon color={color} id={`zd-svg-icon-26-wordmark-${product}`} />
+            <Icon color={color} id="zd-svg-icon-26-wordmark-sunshine" />
           </StyledSpan>
         </Col>
         <Col md={6} style={{ marginBottom: 20 }}>
           <Icon color={color} id="zd-svg-icon-26-wordmark-zendesk" size={size} />
           <span style={{ display: 'inline-block', marginLeft: '.22em', fontSize: size }}>
-            <Icon color={color} id={`zd-svg-icon-26-wordmark-${product}`} />
+            <Icon color={color} id="zd-svg-icon-26-wordmark-sunshine" />
           </span>
         </Col>
       </Row>
-    ))}
-    <Row>
-      <Col md={6} style={{ marginBottom: 20 }}>
-        <Icon
-          color={color || PALETTE.product.talk}
-          fill={fill}
-          id="zd-svg-icon-26-sunshine"
-          size={size}
-        />
-        <StyledSpan size={size}>
-          <Icon color={color} id="zd-svg-icon-26-wordmark-sunshine" />
-        </StyledSpan>
-      </Col>
-      <Col md={6} style={{ marginBottom: 20 }}>
-        <Icon color={color} id="zd-svg-icon-26-wordmark-zendesk" size={size} />
-        <span style={{ display: 'inline-block', marginLeft: '.22em', fontSize: size }}>
-          <Icon color={color} id="zd-svg-icon-26-wordmark-sunshine" />
-        </span>
-      </Col>
-    </Row>
-    <Row>
-      <Col md={6} style={{ marginBottom: 20 }}>
-        <Icon
-          color={color || PALETTE.green[400]}
-          fill={fill}
-          id="zd-svg-icon-26-garden"
-          size={size}
-        />
-        <StyledSpan size={size}>
-          <Icon color={color} id="zd-svg-icon-26-wordmark-garden" />
-        </StyledSpan>
-      </Col>
-      <Col md={6} style={{ marginBottom: 20 }}>
-        <Icon color={color} id="zd-svg-icon-26-wordmark-zendesk" size={size} />
-        <span style={{ display: 'inline-block', marginLeft: '.22em', fontSize: size }}>
-          <Icon color={color} id="zd-svg-icon-26-wordmark-garden" />
-        </span>
-      </Col>
-    </Row>
-    <Row>
-      <Col md={6} style={{ marginBottom: 20 }}>
-        <Icon
-          color={color || PALETTE.kale[700]}
-          fill={fill}
-          id="zd-svg-icon-26-zendesk"
-          size={size}
-        />
-        <StyledSpan size={size} style={{ marginTop: '.14em' }}>
-          <Icon color={color} id="zd-svg-icon-26-wordmark-capital-the" />
-          &nbsp;
-          <Icon color={color} id="zd-svg-icon-26-wordmark-capital-zendesk" />
-          &nbsp;
-          <Icon color={color} id="zd-svg-icon-26-wordmark-capital-suite" />
-        </StyledSpan>
-      </Col>
-      <Col md={6} style={{ marginBottom: 20 }}>
-        <Icon color={color} id="zd-svg-icon-26-wordmark-capital-zendesk" size={size} />
-        <span style={{ display: 'inline-block', marginLeft: '.22em', fontSize: size }}>
-          <Icon color={color} id="zd-svg-icon-26-wordmark-capital-suite" />
-        </span>
-      </Col>
-    </Row>
-  </Grid>
-);
-
-WordmarkLayouts.args = {
-  ...args
-};
-
-WordmarkLayouts.argTypes = {
-  ...argTypes,
-  size: {
-    ...argTypes.size,
-    control: {
-      ...argTypes.size.control,
-      min: 16,
-      max: 48
+      <Row>
+        <Col md={6} style={{ marginBottom: 20 }}>
+          <Icon
+            color={color || PALETTE.green[400]}
+            fill={fill}
+            id="zd-svg-icon-26-garden"
+            size={size}
+          />
+          <StyledSpan size={size}>
+            <Icon color={color} id="zd-svg-icon-26-wordmark-garden" />
+          </StyledSpan>
+        </Col>
+        <Col md={6} style={{ marginBottom: 20 }}>
+          <Icon color={color} id="zd-svg-icon-26-wordmark-zendesk" size={size} />
+          <span style={{ display: 'inline-block', marginLeft: '.22em', fontSize: size }}>
+            <Icon color={color} id="zd-svg-icon-26-wordmark-garden" />
+          </span>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6} style={{ marginBottom: 20 }}>
+          <Icon
+            color={color || PALETTE.kale[700]}
+            fill={fill}
+            id="zd-svg-icon-26-zendesk"
+            size={size}
+          />
+          <StyledSpan size={size} style={{ marginTop: '.14em' }}>
+            <Icon color={color} id="zd-svg-icon-26-wordmark-capital-the" />
+            &nbsp;
+            <Icon color={color} id="zd-svg-icon-26-wordmark-capital-zendesk" />
+            &nbsp;
+            <Icon color={color} id="zd-svg-icon-26-wordmark-capital-suite" />
+          </StyledSpan>
+        </Col>
+        <Col md={6} style={{ marginBottom: 20 }}>
+          <Icon color={color} id="zd-svg-icon-26-wordmark-capital-zendesk" size={size} />
+          <span style={{ display: 'inline-block', marginLeft: '.22em', fontSize: size }}>
+            <Icon color={color} id="zd-svg-icon-26-wordmark-capital-suite" />
+          </span>
+        </Col>
+      </Row>
+    </Grid>
+  ),
+  args,
+  argTypes: {
+    ...argTypes,
+    size: {
+      ...argTypes.size,
+      control: {
+        ...argTypes.size.control,
+        min: 16,
+        max: 48
+      }
     }
-  }
-};
-
-WordmarkLayouts.propTypes = {
-  color: PropTypes.string,
-  fill: PropTypes.string,
-  size: PropTypes.number
+  },
+  propTypes
 };
